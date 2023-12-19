aa865a5048e3bcb9252d1a3980be38a5656b005c
GFORJ1973 <gustavoflores.inf@gmail.com>
2023-12-19 10:14:38 -0300

    Ajustes código

--- mscartoes/src/main/java/br/com/hgl/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/br/com/hgl/mscartoes/application/CartoesResource.java
@@ -1,15 +1,28 @@
 package br.com.hgl.mscartoes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.hgl.mscartoes.application.representation.CartaoSaveRequest;
+import br.com.hgl.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("cartoes")
+@RequiredArgsConstructor
 public class CartoesResource {
 
+
+    private final CartaoService cartaoService;
     @GetMapping
     public String status(){
         return "OK";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request){
+        Cartao cartao = request.toModel();
+        cartaoService.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
