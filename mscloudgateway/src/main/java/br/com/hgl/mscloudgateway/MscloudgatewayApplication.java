43f2897bd4b111ae219b5874b02f1d0cf63611f1
GFORJ1973 <gustavoflores.inf@gmail.com>
2023-12-20 01:38:02 -0300

    Ajustes

--- mscloudgateway/src/main/java/br/com/hgl/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/br/com/hgl/mscloudgateway/MscloudgatewayApplication.java
@@ -19,8 +19,9 @@ public class MscloudgatewayApplication {
     public RouteLocator routes(RouteLocatorBuilder builder){
         return builder
                 .routes()
-                .route(r -> r.path("/clientes/**").uri("lb://msclientes"))
-                .route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
+                    .route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+                    .route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
+                    .route(r -> r.path("/avaliacoes-credito/**").uri("lb://msavaliadorcredito"))
                 .build();
     }
 }
